b043a8a73da44ae7c76903b4b7e39c5a7fe785b2
MortieBroers <47774222+MortieBroers@users.noreply.github.com>
2019-06-04 09:14:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title></title>
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
