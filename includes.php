ac018f8e2f6fff48c62b5828b4428bcedadd2a1b
Mark Wouters van den Oudenweijer <400031747@st.roc.a12.nl>
2019-06-04 12:53:33 +0200

    Include

---
+++ includes.php
@@ -0,0 +1,12 @@
+<?php
+ $host = "remotemysql.com";
+ $dbname = "hNV884Qbj8";
+ $user = "hNV884Qbj8";
+ $password = "OlkJxkax8O";
+
+ $DBH = new PDO('mysql:host=' .$host. ';dbname=' .$dbname, $user, $password);
+
+
+ 
+?>
+
