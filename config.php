fcdaddd572123170195c3912670b3b24c9491850
Mark Wouters van den Oudenweijer <400031747@st.roc.a12.nl>
2019-06-05 12:16:07 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
-function connection()
-{
-    $host =         "remotemysql.com";
-    $username =     "hNV884Qbj8";
-    $password  =    "OlkJxkax8O";
-    $dbname =       "hNV884Qbj8";
-    $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    );
-    return new PDO("mysql:host=$host;dbname=$dbname", "$username", "$password", $options);
-}
+ $host = "localhost";
+ $dbname = "hNV884Qbj8";
+ $user = "hNV884Qbj8";
+ $password = "OlkJxkax8O";
+
+ $DBH = new PDO('mysql:host=' .$host. ';dbname=' .$dbname, $user, $password);
+
+
+
+?>
