14d5566353acbfcd6502153f1990396e241d3b4c
MortieBroers <47774222+MortieBroers@users.noreply.github.com>
2019-06-05 14:02:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ include 'config.php';
 <html lang="en" dir="ltr">
 
 <head>
-  <meta charset="utf-8">
   <title>DE GAME</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
@@ -31,48 +33,48 @@ include 'config.php';
 
       </div>
     </div>
-    </header>
-    <button id="openButton" onclick="openInfo()">Open Info</button>
+  </header>
+  <button id="openButton" onclick="openInfo()">Open Info</button>
 
-    <button id="closeButton" onclick="closeInfo()">Close Info</button>
+  <button id="closeButton" onclick="closeInfo()">Close Info</button>
 
-    <div id="info">
-      <h6>Hoe Werkt Dit Spel?</h4>
+  <div id="info">
+    <h6>Hoe Werkt Dit Spel?</h4>
       <p>
         Info
       </p>
 
-      <h6>Geef De Spelers Een Boost.</h4>
+    <h6>Geef De Spelers Een Boost.</h4>
       <p>
         Boost
       </p>
 
-      <h6>Spawn Vijanden In Het Spel.</h4>
+    <h6>Spawn Vijanden In Het Spel.</h4>
       <p>
         Enemy
       </p>
 
-      <h6>Verblind De Spelers.</h4>
+    <h6>Verblind De Spelers.</h4>
       <p>
         Vignette
       </p>
-    </div>
+  </div>
 
-    <form type="$_POST">
-      <input type="submit" name="Boost" value="Boost Speler">
-      <input type="submit" name="Enemy" value="Spawn Enemy">
-      <input type="submit" name="Blindness" value="Verblind Speler">
-    </form>
+  <form type="$_POST">
+    <input type="submit" name="Boost" value="Boost Speler">
+    <input type="submit" name="Enemy" value="Spawn Enemy">
+    <input type="submit" name="Blindness" value="Verblind Speler">
+  </form>
 
-    <script src="script.js"></script>
+  <script src="script.js"></script>
 
-    <?php
-    $cat = "SELECT * FROM gameserver";
-    $antwoord = $DBH->query($cat);
-    while ($row = $antwoord->fetch(PDO::FETCH_ASSOC)) {
-      print $row['score'];
-    }
-    ?>
+  <?php
+  $cat = "SELECT * FROM gameserver";
+  $antwoord = $DBH->query($cat);
+  while ($row = $antwoord->fetch(PDO::FETCH_ASSOC)) {
+    print $row['score'];
+  }
+  ?>
 
 </body>
 
