137159afafb5feb0db7a5c450cffb905e394c108
MortieBroers <47774222+MortieBroers@users.noreply.github.com>
2019-06-05 12:00:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,28 +5,16 @@
   <title>DE GAME</title>
 </head>
   <body>
+    <?php
+      $cat = "SELECT * FROM gameserver ";
+      $antwoord = $DBH->query($cat);
+      while ($row = $antwoord->fetch(PDO::FETCH_ASSOC)) {
 
+        print $row['score'];
+      }
+    ?>
   </body>
 </html>
 <?php
-include 'includes.php';
- ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-$cat = "SELECT * FROM gameserver ";
-$antwoord = $DBH->query($cat);
-while ($row = $antwoord->fetch(PDO::FETCH_ASSOC)) {
-
-  print $row['score'];
-
-}
+  include 'includes.php';
 ?>
-
-  </body>
-</html>
