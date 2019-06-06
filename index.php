ced18971ef03365ed703a09ed6507406a3b72e59
MortieBroers <47774222+MortieBroers@users.noreply.github.com>
2019-06-06 14:04:24 +0200

    u

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-include 'config.php';
+require_once 'function.php';
 ?>
 <html lang="en" dir="ltr">
 
 <head>
-  <meta charset="utf-8">
   <title>DE GAME</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="../style.css" />
   <link href="https://fonts.googleapis.com/css?family=Ruge+Boogie&display=swap" rel="stylesheet">
 </head>
 
@@ -47,35 +50,48 @@ include 'config.php';
     </div>
     <div class="right-header">
       <div class="wrapper-right-header">
+
         <button id="openButton" onclick="openInfo()">Open Info</button>
         <button id="closeButton" onclick="closeInfo()">Close Info</button>
+
         <div id="info">
-          <h4>Hoe Werkt Dit Spel?</h4>
-          <p>
-            Info
-          </p>
+          <h6>Hoe Werkt Deze Site?</h4>
+            <p>
+              Op deze site kun je bepaalde gebeurtenissen besturen die in het spel gebeuren.<\br>
+              De spelers lijden onder die gebeurtenissen.
+            </p>
 
-          <h4>Geef De Spelers Een Boost.</h4>
-          <p>
-            Boost
-          </p>
+          <h6>Geef De Spelers Een Boost.</h4>
+            <p>
+              De spelers krijgen een snelheidsboost die 5 seconden lang duurt.<\br>
+              <\br>
+              (dit heeft een cooldown van 20 seconden)
+            </p>
 
-          <h4>Spawn Vijanden In Het Spel.</h4>
-          <p>
-            Enemy
-          </p>
+          <h6>Spawn Vijanden In Het Spel.</h4>
+            <p>
+              De spelers worden verhinderd door tegenliggers. Als de speler tegen de tegenligger crasht, verliest de speler.<\br>
+              <\br>
+              (dit heeft een cooldown van 20 seconden)
+            </p>
 
-          <h4>Verblind De Spelers.</h4>
-          <p>
-            Vignette
-          </p>
+          <h6>Verblind De Spelers.</h4>
+            <p>
+              De spelers krijgen een verblindend effect op hun beeld.<\br>
+              <\br>
+              (dit heeft een cooldown van 20 seconden)
+            </p>
         </div>
+
       </div>
     </div>
-    </header>
-    <div class="application-holder">
-
-    </div>
-    <script src="script.js"></script>
+  </header>
+  <table id="application-holder">
+    <?php
+      score($DBH);
+    ?>
+  </table>
+  <script src="script.js"></script>
 </body>
+
 </html>
